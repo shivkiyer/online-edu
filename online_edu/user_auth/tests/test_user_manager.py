5a63b5818c9019c627f8cb62e43bf877c295657b
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-01-22 17:51:16 -0500

    Tests for user manager

---
+++ online_edu/user_auth/tests/test_user_manager.py
@@ -0,0 +1,79 @@
+import pytest
+import time
+
+from .fixtures import test_user, \
+    test_configurable_user, \
+    verification_token
+from user_auth.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+def test_get_user_by_id(test_configurable_user):
+    '''Test the basic get_user_by_id query'''
+
+    user1 = test_configurable_user(
+        'someuser@domain.com',
+        'somepassword',
+        False
+    )
+    user2 = test_configurable_user(
+        'anotheruser@domain.com',
+        'anotherpassword',
+        False
+    )
+
+    fetch_user = User.objects.get_user_by_id(1)
+    assert fetch_user.username == 'someuser@domain.com'
+
+    fetch_user = User.objects.get_user_by_id(2)
+    assert fetch_user.username == 'anotheruser@domain.com'
+
+    with pytest.raises(Exception) as e:
+        fetch_user = User.objects.get_user_by_id(3)
+    assert str(e.value) == 'User not found'
+
+
+def test_get_user_by_token(test_user, verification_token):
+    '''Test extracting user from JWT token'''
+
+    user1 = test_user
+
+    token1 = verification_token(60)
+    fetch_user = User.objects.get_user_by_token(str(token1))
+    assert fetch_user.username == user1.username
+
+    token2 = verification_token(1)
+    time.sleep(2)
+    with pytest.raises(Exception) as e:
+        fetch_user = User.objects.get_user_by_token(token2)
+    assert str(e.value) == 'Token is invalid or expired'
+
+
+def test_get_user_by_email(test_user):
+    '''Test extracting user from email'''
+
+    user1 = test_user
+
+    fetch_user = User.objects.get_user_by_email(test_user.username)
+    assert fetch_user.username == user1.username
+
+    with pytest.raises(Exception) as e:
+        fetch_user = User.objects.get_user_by_email('anotheruser@domain.com')
+    assert str(e.value) == 'User not found'
+
+
+def test_active_user_by_token(test_user, verification_token):
+    '''Test activating user from verification token'''
+
+    user1 = test_user
+
+    token1 = verification_token(60)
+    fetch_user = User.objects.activate_user_by_token(str(token1))
+    assert fetch_user.is_active == True
+
+    token2 = verification_token(1)
+    time.sleep(2)
+    with pytest.raises(Exception) as e:
+        fetch_user = User.objects.activate_user_by_token(token2)
+    assert str(e.value) == 'User could not be activated'
