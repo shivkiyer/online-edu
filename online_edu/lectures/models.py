8dc7778d47feb4cb41139b073a12489604f0a12d
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-02-12 15:46:26 -0500

    Adding seq_no to lecture model

--- online_edu/lectures/models.py
+++ online_edu/lectures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
 from .managers import LectureManager
 
@@ -15,8 +16,23 @@ class Lecture(models.Model):
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    seq_no = models.IntegerField(default=0)
 
     objects = LectureManager()
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['seq_no']
+
+
+def generate_sequence_no(sender, instance, *args, **kwargs):
+    '''Generate sequence number for a lecture in a course'''
+    if not instance.seq_no:
+        course = instance.course
+        last_lecture_no = sender.objects.filter(course=course).count()
+        instance.seq_no = last_lecture_no + 1
+
+
+pre_save.connect(generate_sequence_no, sender=Lecture)
